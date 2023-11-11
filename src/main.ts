034f6a443d5812b7d6765f06997ec74224a03157
leaf-114514 <54039083@qq.com>
2023-11-11 17:04:39 +0800

    懒加载的优化

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+import { lazyPlugin } from './directlives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
-
-app.directive('img-lazy', {
-    mounted(el, binding){
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                if (isIntersecting) {
-                    el.src = binding.value
-                }
-            }
-        )
-    }
-})
