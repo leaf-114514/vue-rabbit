a58bf22a3eb48037f55ec5aa8371de8653464b11
leaf-114514 <54039083@qq.com>
2023-11-11 15:20:10 +0800

    home-新鲜好物的实现

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -1,9 +1,24 @@
 import httpInstance from "@/utils/http";
 
 
-//banner
-export function getBannerAPI(){
+/**
+ * @description: 获取轮播图
+ * @param {*}
+ * @return {*}
+ */
+export const getBannerAPI = () => {
     return httpInstance({
         url: '/home/banner'
     })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+ export const findNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+  }
